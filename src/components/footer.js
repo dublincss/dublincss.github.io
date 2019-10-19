361d6fca868e9634ef5b8d8e4a125892c6b2dee7
jorgbot <jbotelloangarita@hotmail.com>
2019-10-19 11:07:14 -0500

    #5 Add icons on the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Twitter from "../images/twitter.svg"
+import Meetup from "../images/meetup.svg"
 
 class Footer extends Component {
   componentDidMount() {
@@ -17,6 +19,12 @@ class Footer extends Component {
           target="_blank"
           rel="noopener noreferrer"
         >DublinCSS community</a> - 2019
+
+        <nav class="site-menu">
+          <a aria-current="page" class="site-menu__item" href="https://twitter.com/DublinCSS" target="_blank"> <img src={Twitter} className="site-header__logoFooter" /> </a>
+          <a class="site-menu__item" href="https://meetup.com/DublinCSS" target="_blank"> <img src={Meetup} className="site-header__logoFooter" /> </a>
+        </nav>
+
       </footer>
     );
   }
